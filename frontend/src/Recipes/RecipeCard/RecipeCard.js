8c57c7ba93d7dfbc2a02c4c663b493919dae9f8f
Said_Alizade <saidjavanshir@gmail.com>
2022-06-30 23:06:42 +0300

    App bar added and Recipe card added

---
+++ frontend/src/Recipes/RecipeCard/RecipeCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button, Card, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+
+const RecipeCard = (name, image, description, recipeId) => {
+    return ( 
+        <Card>
+            <CardHeader title={name} />
+            <CardMedia component="img" image={image} alt={name} />
+            <CardContent>
+                <Typography>{description}</Typography>
+            </CardContent>
+            <Button variant="contained">Open</Button>
+        </Card>
+     );
+}
+ 
+export default RecipeCard;
